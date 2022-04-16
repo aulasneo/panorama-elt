41f709904a2cc69d8cbe283a408d559ecddbdea0
Andrés González <andres@aulasneo.com>
2022-04-16 15:49:33 -0300

    Improve shell template

--- openedx_extract_and_load_example.sh
+++ openedx_extract_and_load_example.sh
@@ -1,22 +1,22 @@
 LMS_HOST=$(hostname)
 
-export PANORAMA_RAW_DATA_BUCKET='panoramabuckets10f2cc5a-rawdata202c7bd0-qsp6ob4zs8pw'
+export PANORAMA_RAW_DATA_BUCKET='panorama-raw-data-bucket'
 export PANORAMA_AWS_ACCESS_KEY=
 export PANORAMA_AWS_SECRET_ACCESS_KEY=
-export PANORAMA_AWS_REGION=
+export PANORAMA_AWS_REGION=us-east-1
 
 export PANORAMA_MYSQL_USERNAME=root
 export PANORAMA_MYSQL_PASSWORD=
 export PANORAMA_MYSQL_HOST=
-export PANORAMA_MYSQL_DATABASE=
+export PANORAMA_MYSQL_DATABASE=edxapp
 
 export PANORAMA_MYSQL_TABLES="auth_user,student_courseenrollment,auth_userprofile,student_courseaccessrole,course_overviews_courseoverview,courseware_studentmodule,grades_persistentcoursegrade,student_manualenrollmentaudit,student_courseenrollmentallowed,certificates_generatedcertificate"
 export PANORAMA_BASE_PARTITIONS="{\"lms\":\"$LMS_HOST\"}"
 export PANORAMA_TABLE_PARTITIONS='{"courseware_studentmodule": {"partition_fields": ["course_id"], "interval": "2 hour", "timestamp_field": "modified", "datalake_db": "panorama", "datalake_table": "courseware_studentmodule_raw", "workgroup": "panorama"}}'
 
 export PANORAMA_MONGODB_USERNAME=edxapp
-export PANORAMA_MONGODB_PASSWORD='iVZBvllQse9yDFa0EciXxeo0C0FzgqyxfN3'
-export PANORAMA_MONGODB_HOST=
-export PANORAMA_MONGODB_DATABASE=
+export PANORAMA_MONGODB_PASSWORD=
+export PANORAMA_MONGODB_HOST=localhost
+export PANORAMA_MONGODB_DATABASE=edxapp
 
-venv/bin/python extract_mysql_tables.py
+venv/bin/python openedx_extract_and_load.py
